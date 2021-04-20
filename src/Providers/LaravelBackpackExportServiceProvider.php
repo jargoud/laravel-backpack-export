6f9ff28c2def4d884e9e987d5ebb182a67c91270
Jérémy ARGOUD <jargoud@webqam.fr>
2021-04-20 16:29:56 +0200

    Add views to service provider

--- src/Providers/LaravelBackpackExportServiceProvider.php
+++ src/Providers/LaravelBackpackExportServiceProvider.php
@@ -6,4 +6,29 @@ use Illuminate\Support\ServiceProvider;
 
 class LaravelBackpackExportServiceProvider extends ServiceProvider
 {
+    public const NAMESPACE = 'laravel-backpack-export';
+
+    /**
+     * Bootstrap the application services.
+     */
+    public function boot()
+    {
+        $this->loadViewsFrom(__DIR__ . '/../resources/views', self::NAMESPACE);
+
+        if ($this->app->runningInConsole()) {
+            $this->publishViews();
+        }
+    }
+
+    protected function publishViews(): self
+    {
+        $this->publishes(
+            [
+                __DIR__ . '/../resources/views' => resource_path('views/vendor/' . self::NAMESPACE),
+            ],
+            'views'
+        );
+
+        return $this;
+    }
 }
