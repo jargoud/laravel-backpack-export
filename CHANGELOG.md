e7756a82f6f701cf91b8259aa66208fc519c0f3d
Jérémy ARGOUD <jargoud@webqam.fr>
2021-04-20 16:28:41 +0200

    Create package

---
+++ CHANGELOG.md
@@ -0,0 +1,7 @@
+# Changelog
+
+All notable changes to `laravel-backpack-export` will be documented in this file
+
+## 1.0.0 - 2021-04-20
+
+- Initial release
---
+++ src/Providers/LaravelBackpackExportServiceProvider.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Jargoud\LaravelBackpackExport\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaravelBackpackExportServiceProvider extends ServiceProvider
+{
+}
